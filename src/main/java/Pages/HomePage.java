032c002abf049f8b500ed06e8061ff4dbcd361f8
kbprasad1978 <kbprasad1978@gmail.com>
2025-09-15 15:42:16 +0100

    Update HomePage.java
    test this file to check if the build is triggered in Jenkins----------------removed this line in GitHub

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -23,8 +23,6 @@ public class HomePage {
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
-
-       test this file to check if the build is triggered in Jenkins
     }
 
     public void goToHomePage(){
